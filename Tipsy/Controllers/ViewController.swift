0b4308297853ec8759b49d720435e859d22b095b
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-30 13:36:03 +0530

    update tip buttonUI when tapped

--- Tipsy/Controllers/ViewController.swift
+++ Tipsy/Controllers/ViewController.swift
@@ -17,8 +17,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipTenButton: UIButton!
     @IBOutlet weak var tipTwentyButton: UIButton!
     // IBActions
-    @IBAction func tipSelected(_ sender: UIButton) {}
+    @IBAction func tipSelected(_ sender: UIButton) {
+        resetButtons()
+        sender.modify(backgroundColor: #colorLiteral(red: 0, green: 0.9485482574, blue: 0.6213100553, alpha: 1), titleColor: .white)
+    }
     @IBAction func splitStepperValueChanged(_ sender: UIStepper) {}
     @IBAction func calculatePressed(_ sender: UIButton) {}
+    
+    // Customise UI
+    private func resetButtons() {
+        tipZeroButton.modify(backgroundColor: .clear, titleColor: #colorLiteral(red: 0, green: 0.9485482574, blue: 0.6213100553, alpha: 1))
+        tipTenButton.modify(backgroundColor: .clear, titleColor: #colorLiteral(red: 0, green: 0.9485482574, blue: 0.6213100553, alpha: 1))
+        tipTwentyButton.modify(backgroundColor: .clear, titleColor: #colorLiteral(red: 0, green: 0.9485482574, blue: 0.6213100553, alpha: 1))
+    }
+}
+
+extension UIButton {
+    func modify(backgroundColor: UIColor, titleColor: UIColor) {
+        self.backgroundColor = backgroundColor
+        setTitleColor(titleColor, for: .normal)
+    }
 }
 
