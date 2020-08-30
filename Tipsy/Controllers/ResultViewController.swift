0932428bd6d1e6c34ffc598fa6a26e4a959d17af
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-30 13:19:31 +0530

    attach ResultViewController

---
+++ Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Nishant Taneja on 30/08/20.
+//  Copyright © 2020 Nishant Taneja. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    // IBOutlets
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    // IBAction
+    @IBAction func recalculatePressed(_ sender: UIButton) {}
+}
